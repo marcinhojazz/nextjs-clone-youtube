21a122d12e5be4d45f04cca5abff77502d33244a
MARCIO MACHADO PONTES <marcinhojazz@gmail.com>
2021-04-13 15:58:33 -0300

    WHITELABEL PROJECTS CONFIG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 
 export default function Home() {
   return (
     <div>
       Clone Youtube com NextJs + Material Ui
+      <Button color="primary">Hello World</Button>
     </div>
-  );
+  )
+
 }
